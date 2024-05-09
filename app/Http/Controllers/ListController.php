d984bdf3e0c4096e84e8885d1cf34071652115b0
joey-0824 <jasperjoey.0824@gmail.com>
2024-05-09 11:04:09 -0400

    make search function works by filtering.

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class ListController extends Controller
 {
     // show all listings
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function index() {
         return view('listings.index', [
-            'listing' => \App\Models\Listing::all(),
+            'listing' => \App\Models\Listing::latest()->filter(request(['tag', 'search']))->get(),
         ]);
     }
 
