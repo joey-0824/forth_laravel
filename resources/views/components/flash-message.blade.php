2f691e9190a701f7e261c444136a3ba67d589104
joey-0824 <jasperjoey.0824@gmail.com>
2024-05-10 11:27:16 -0400

    install alphinejs and use x-show to auto display and hide message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,6 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 left-1/2 transform -trnaslate-x-1/2 bg-laravel text-white px-48 py-3">
+    <div x-data="{show: true}" x-show="show" x-init="setTimeout(()=>show=false, 3000)"
+        class="fixed top-0 left-1/2 transform -trnaslate-x-1/2 bg-laravel text-white px-48 py-3">
         <p>{{session('message')}}</p>
     </div>
 @endif
