91fe921f28d4f6eb567d4f9d286ec84297e51fe5
joey-0824 <jasperjoey.0824@gmail.com>
2024-05-10 16:32:41 -0400

    upload logo file by form file picker, and store it into database, also public it in the storage folder

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -45,6 +45,10 @@ class ListController extends Controller
             'description' => 'required'
         ]);
 
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'New Listing Created Successfully.');
